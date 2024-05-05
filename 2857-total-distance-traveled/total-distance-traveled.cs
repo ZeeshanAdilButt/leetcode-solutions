c6ef43ebe7416d0ed95fad8b67b21eec63bab37d
Zeeshan Adil <zeeabutt@outlook.com>
2024-05-05 17:30:02 -0400

    Time: 48 ms (18.97%) | Memory: 30.6 MB (55.17%) - LeetSync

---
+++ 2857-total-distance-traveled/total-distance-traveled.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int DistanceTraveled(int mainTank, int additionalTank) {
+        
+        int result = 0;
+        for (int i = 1; i <= mainTank; i++) {
+            if (additionalTank != 0 && i % 5 == 0) {
+                additionalTank -= 1;
+                result += 10;
+                mainTank += 1;
+            }
+            else {
+                result += 10;
+            }
+        }
+        return result;
+    }
+    
+}
