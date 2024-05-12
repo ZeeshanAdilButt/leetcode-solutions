4efba7d92a7706efdc6736b1200c7a68b820f5bd
Zeeshan Adil <zeeabutt@outlook.com>
2024-05-12 08:19:52 -0400

    Time: 432 ms (5.41%) | Memory: 64.1 MB (5.41%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cs
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cs
@@ -1,5 +1,6 @@
 public class Solution {
-   public int MaximumCandies(int[] candies, long k) {
+    
+public int MaximumCandies(int[] candies, long k) {
         if (candies.Sum(x => (long)x) < k)
             return 0;
         int left = 1;
@@ -13,5 +14,38 @@ public class Solution {
         }
         return left;
     }
+    
+// public int MaximumCandies(int[] candies, long numChildren) {
+//     // Define the search space
+//     int left = 1; // Minimum candies allocated to each child
+//     int total = candies.Sum(); // Maximum candies allocated to each child
+//     int right  = total; 
+    
+//     // Binary search to find the maximum candies
+//     while (left < right) {
+//         int mid = left + (right - left) / 2;
+        
+//         // Check if it's possible to allocate candies to K children
+//         if (CanAllocateCandies(total, mid, numChildren)) {
+//             right = mid; // Search in the lower half
+//         } else {
+//             left = mid + 1; // Search in the upper half
+//         }
+//     }
+    
+//     // The maximum candies found is 'left' as 'left' will be the smallest value that satisfies the condition
+//     return left;
+// }
+
+// // Helper function to check if it's possible to allocate candies to K children
+// private bool CanAllocateCandies(int totalcandies, int candiestoAllocate, long numChildren) {
+    
+//     if((totalcandies/candiestoAllocate) >= numChildren)
+//         return true;
+    
+    
+//     return false;
+// }
+
 
 }
