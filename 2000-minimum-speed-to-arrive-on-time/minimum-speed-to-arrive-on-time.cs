29187b3018fb518ae1ca438ca5914ed5eb1c35ef
Zeeshan Adil <zeeabutt@outlook.com>
2024-05-12 08:27:46 -0400

    Time: 433 ms (97.50%) | Memory: 69.7 MB (5.00%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int MinSpeedOnTime(int[] dist, double hour) {
+        int speed =-1;
+        int left = 1;
+        int right = 10000000;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            double time = helper(dist, mid);
+            if(time <= hour) {
+                speed = mid;
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+
+        return speed;
+    }
+
+    private double helper(int[] dist, int speed) {
+        double time = 0;
+        for(int i=0; i<dist.Length; i++) {
+            double t = (double)dist[i] / speed;
+            time += (i == dist.Length-1 ? t : Math.Ceiling(t));
+        }
+
+        return time;
+    }
+}
