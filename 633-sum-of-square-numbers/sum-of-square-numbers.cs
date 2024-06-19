1e4d6c7f136a7d82e10d96272aa96c7b99288255
Zeeshan Adil <zeeabutt@outlook.com>
2024-06-18 20:20:09 -0400

    Time: 37 ms (35.38%) | Memory: 29.1 MB (16.43%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cs
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cs
@@ -1,16 +1,45 @@
+// public class Solution {
+//     public bool JudgeSquareSum(int c) {
+        
+//         HashSet<int> set = new HashSet<int>();
+        
+//         for (int i = 0; i<= Math.Sqrt(c); i++){
+            
+            
+//             set.Add(i*i);
+            
+//             if (set.Contains( c - (i * i)))
+//                 return true;
+            
+            
+//         }
+        
+//         return false;
+        
+//     }
+// }
+
+//two pointer
+
 public class Solution {
     public bool JudgeSquareSum(int c) {
         
-        HashSet<int> set = new HashSet<int>();
+        int left = 0;
+        long right =  (long)Math.Sqrt(c);
         
-        for (int i = 0; i<= Math.Sqrt(c); i++){
+        while (left<=right){
             
+           long current = left*left + right*right;
             
-            set.Add(i*i);
-            
-            if (set.Contains( c - (i * i)))
+            if (current > c){
+                right--;
+            }
+            else if (current < c){
+                left++;
+            }
+            else{
                 return true;
-            
+            }
             
         }
         
