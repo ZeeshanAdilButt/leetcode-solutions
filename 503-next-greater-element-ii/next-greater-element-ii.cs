427b72bf9d646420e32edbf01dd4b33cafc69589
Zeeshan Adil <zeeabutt@outlook.com>
2024-06-23 17:13:21 -0400

    Time: 142 ms (79.72%) | Memory: 59.2 MB (79.72%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cs
+++ 503-next-greater-element-ii/next-greater-element-ii.cs
@@ -1,22 +1,45 @@
 public class Solution {
     public int[] NextGreaterElements(int[] nums) {
             
-        int[] arr = new int[nums.Length];
+        int[] res = new int[nums.Length];
         
         for (int i =0; i < nums.Length; i++){
-            arr[i] = -1;
+            res[i] = -1;
         }
         
-        for (int i =0; i < nums.Length; i++){
+        Stack<int> stack = new Stack<int>();
+        
+        for (int i =0; i < nums.Length * 2 ; i++){
             
-            for (int j = 1; j < nums.Length; j++){
-                if(nums [(i + j) % nums.Length] > nums [i] ){
-                    arr[i] = nums [(i + j) % nums.Length];
-                    break;
-                }    
-            }             
+            
+            while(stack.Count!=0 && nums[stack.Peek()] < nums[i% nums.Length]){
+                
+                res[stack.Pop()] = nums[i % nums.Length];
+            }
+            
+            stack.Push(i % nums.Length);
         }
         
-        return arr;        
+        return res;        
     }
+    
+//     public int[] NextGreaterElements(int[] A)
+//     {
+//         int n = A.Length;
+//         int[] res = new int[n];
+//         Array.Fill(res, -1);
+//         Stack<int> stack = new Stack<int>();
+        
+//         for (int i = 0; i < n * 2; i++)
+//         {
+//             while (stack.Count > 0 && A[stack.Peek()] < A[i % n])
+//             {
+//                 res[stack.Pop()] = A[i % n];
+//             }
+//             stack.Push(i % n);
+//         }
+        
+//         return res;
+//     }
 }
+
