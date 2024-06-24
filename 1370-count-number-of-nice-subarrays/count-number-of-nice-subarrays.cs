351409a270d728822fd296809e6c3493f874ef77
Zeeshan Adil <zeeabutt@outlook.com>
2024-06-23 22:04:52 -0400

    Time: 309 ms (79.30%) | Memory: 54.9 MB (30.11%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cs
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cs
@@ -5,18 +5,21 @@ public class Solution {
         
         for (int i = 0; i < nums.Length; i++) {
             
-            if (nums[i] % 2 !=0) {
+            if (nums[i] % 2 == 1) {
                 odd++;
-                
                 if (odd >= k) {
+                    
                     count = 1;
+                    
                     while (nums[j++] % 2 == 0) 
                         count++;
+                    
                     total += count;
                 }
             } 
-            else if (odd >= k)
+            else if (odd >= k) {
                 total += count;
+            }
         }
         return total;
     }
