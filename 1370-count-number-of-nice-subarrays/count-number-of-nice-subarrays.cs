057a440953917464d87539948f620c32ade706ca
Zeeshan Adil <zeeabutt@outlook.com>
2024-06-23 21:31:10 -0400

    Time: 314 ms (64.79%) | Memory: 51.1 MB (95.97%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int NumberOfSubarrays(int[] nums, int k) {
+        
+        int j = 0, odd = 0, count = 0, total = 0;
+        
+        for (int i = 0; i < nums.Length; i++) {
+            
+            if (nums[i] % 2 !=0) {
+                odd++;
+                
+                if (odd >= k) {
+                    count = 1;
+                    while (nums[j++] % 2 == 0) 
+                        count++;
+                    total += count;
+                }
+            } 
+            else if (odd >= k)
+                total += count;
+        }
+        return total;
+    }
+}
