852db658bd15829ed95afd5be295e6166028039a
Zeeshan Adil <zeeabutt@outlook.com>
2024-06-27 20:20:22 -0400

    Time: 100 ms (68.02%) | Memory: 45.4 MB (69.70%) - LeetSync

---
+++ 136-single-number/single-number.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        
+        int num =0;
+        
+        for (int i=0; i < nums.Length; i++){
+            
+            num ^= nums[i];
+        }
+        
+        return num;
+        
+    }
+}
