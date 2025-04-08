d0bbce0f4f13e06ad54c37437fa2dc0b88c430a4
Zeeshan Adil <zeeabutt@outlook.com>
2025-04-08 16:22:38 +0500

    Time: 3 ms (97.36%) | Memory: 68.7 MB (62.31%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cs
+++ 234-palindrome-linked-list/palindrome-linked-list.cs
@@ -10,23 +10,77 @@
  * }
  */
 public class Solution {
-    public bool IsPalindrome(ListNode head) {
-        List<int> arr = new List<int>();
-        ListNode cur = head;
-        while (cur != null) {
-            arr.Add(cur.val);
-            cur = cur.next;
-        }
+    // public bool IsPalindrome(ListNode head) {
+    //     List<int> arr = new List<int>();
+    //     ListNode cur = head;
+    //     while (cur != null) {
+    //         arr.Add(cur.val);
+    //         cur = cur.next;
+    //     }
 
-        int l = 0, r = arr.Count - 1;
-        while (l < r) {
-            if (arr[l] != arr[r]) {
+    //     int l = 0, r = arr.Count - 1;
+    //     while (l < r) {
+    //         if (arr[l] != arr[r]) {
+    //             return false;
+    //         }
+    //         l++;
+    //         r--;
+    //     }
+
+    //     return true;
+    // }
+
+    // Function to check if a linked list is a palindrome
+    public bool IsPalindrome(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null)
+        {            
+            slow = slow.next;           
+            fast = fast.next.next;
+        }
+        
+        ListNode revertData = ReverseLinkedList(slow);     
+        bool check = CompareTwoHalves(head, revertData);
+        // LinkedListReversal.ReverseLinkedList(revertData);
+
+        return check;
+    }
+
+    // Function to compare two halves of a linked list
+    public static bool CompareTwoHalves(ListNode firstHalf, ListNode secondHalf)
+    {        
+        while (firstHalf != null && secondHalf != null)
+        {
+            if (firstHalf.val != secondHalf.val)
+            {
                 return false;
             }
-            l++;
-            r--;
+            else
+            {
+                firstHalf = firstHalf.next;
+                secondHalf = secondHalf.next;
+            }
+        }
+        return true;
+    }
+
+     public static ListNode ReverseLinkedList(ListNode slowPtr)
+    {
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = slowPtr;
+
+        while (curr != null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        return true;
+        return prev;
     }
 }
