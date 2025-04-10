4351faf167ed5e6407046a1736683bb203358b77
Zeeshan Adil <zeeabutt@outlook.com>
2025-04-10 18:38:23 +0500

    Time: 2 ms (95.96%) | Memory: 58.1 MB (72.73%) - LeetSync

---
+++ 1028-interval-list-intersections/interval-list-intersections.cs
@@ -0,0 +1,24 @@
+public class Solution {
+
+    public int[][] IntervalIntersection(int[][] intervalLista, int[][] intervalListb)
+    {
+        List<int[]> intersections = new List<int[]>();
+        int i = 0, j = 0;
+
+        while (i < intervalLista.Length && j < intervalListb.Length)
+        {
+            int start = Math.Max(intervalLista[i][0], intervalListb[j][0]);
+            int end = Math.Min(intervalLista[i][1], intervalListb[j][1]);
+
+            if (start <= end)
+                intersections.Add(new int[] { start, end });
+            if (intervalLista[i][1] < intervalListb[j][1])
+                i += 1;
+            else
+                j += 1;
+        }
+
+        return intersections.ToArray();
+    }
+   
+}
