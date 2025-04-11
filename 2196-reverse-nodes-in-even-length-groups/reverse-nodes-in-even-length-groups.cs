9505d42b7bc3e4692ed2f29bd37f2de4e1501f76
Zeeshan Adil <zeeabutt@outlook.com>
2025-04-11 17:07:24 +0500

    Time: 5 ms (100.00%) | Memory: 82.3 MB (60.00%) - LeetSync

---
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.cs
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+
+        public ListNode ReverseEvenLengthGroups(ListNode head)
+    {
+        ListNode prev = head; 
+        ListNode node, reverse, currNext, curr, prevNext = null;
+        int groupLen = 2; 
+        int numNodes = 0;
+        while (prev.next != null)
+        {
+            node = prev;
+            numNodes = 0;
+            for (int i = 0; i < groupLen; i++)
+            {
+                if (node.next == null)
+                    break;
+                numNodes += 1;
+                node = node.next;
+            }
+
+            if (numNodes % 2 != 0) 
+            {
+                prev = node;
+            }
+            else
+            {
+                reverse = node.next;
+                curr = prev.next;
+                for (int j = 0; j < numNodes; j++)
+                {
+                    currNext = curr.next;
+                    curr.next = reverse;
+                    reverse = curr;
+                    curr = currNext;
+                }
+                prevNext = prev.next;
+                prev.next = node;
+                prev = prevNext;
+            }
+            groupLen += 1;
+        }
+        return head;
+    }
+}
