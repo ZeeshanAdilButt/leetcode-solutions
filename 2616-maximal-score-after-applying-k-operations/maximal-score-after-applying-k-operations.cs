a8a3a0c3ddd4cd84720374d652a52d9b3e530338
Zeeshan Adil <zeeabutt@outlook.com>
2025-04-14 17:56:48 +0500

    Time: 342 ms (29.41%) | Memory: 84.1 MB (5.88%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cs
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cs
@@ -23,34 +23,28 @@ public class Solution {
     //     return score;
     // }
 
-    public long MaxKelements(int[] nums, int k) {
-    
-        // Create a max heap using a priority queue
-        PriorityQueue<int, int> pq = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b - a));
-        
-        // Add elements to the priority queue
-        foreach (int x in nums)
-        {
-            pq.Enqueue(x, x);
-        }
-        
-        long score = 0;
-        
-        // Process the queue while there are elements and k is greater than 0
-        while (pq.Count > 0 && k > 0)
-        {
-            int x = pq.Dequeue();
-            score += x;
-            
-            // Add the next element as the ceiling of x/3
-            int newElement = (int)Math.Ceiling(x / 3.0);
-            pq.Enqueue(newElement, newElement);
-            
-            k--;
-        }
-        
-        return score;
+    public long MaxKelements(int[] nums, int k)
+    {
+        // Simulate max-heap using -num as priority
+        var maxHeap = new PriorityQueue<long, long>();
 
-    
+        foreach (int num in nums)
+        {
+            long val = num;
+            maxHeap.Enqueue(val, -val); // max-heap using negative priority
+        }
+
+        long score = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            long largest = maxHeap.Dequeue();
+            score += largest;
+
+            long reduced = (int)Math.Ceiling(largest / 3.0);
+            maxHeap.Enqueue(reduced, -reduced);
+        }
+
+        return score;
     }
 }
