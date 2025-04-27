2b4eb4d11a0fa46e45e040c8a79b41c8a47a11cf
Zeeshan Adil <zeeabutt@outlook.com>
2025-04-28 03:16:27 +0500

    Time: 16 ms (80.03%) | Memory: 45.7 MB (59.47%) - LeetSync

--- 322-coin-change/coin-change.cs
+++ 322-coin-change/coin-change.cs
@@ -1,45 +1,70 @@
 public class Solution {
-    private Dictionary<int, int> memo = new Dictionary<int, int>();
 
-    private int Dfs(int amount, int[] coins) {
-
-        if (amount == 0) 
-            return 0;
-
-            
-        if (amount < 0) 
-            return  int.MaxValue;
+    // public int CoinChange(int[] coins, int amount) {
         
-        if (memo.ContainsKey(amount)) 
-            return memo[amount];
+    //     Array.Sort(coins);
+    //     Array.Reverse(coins);
 
-        int res = int.MaxValue;
+    //     int minCoins = Dfs(amount, coins);
+
+    //     return minCoins == int.MaxValue ? -1 : minCoins;
+    // }
+
+   public int CoinChange(int[] coins, int total) {
         
-        foreach (int coin in coins) {
-            if (coin <= amount) {
-                
-               int result = Dfs(amount - coin, coins);
+        int[] dp = new int[total+ 1];
 
-               if( result != int.MaxValue){
-                    res = Math.Min(res, 1+ result);
+        Array.Fill(dp, total + 1);
 
-               }
+        dp[0] = 0;
 
-               
+        for (int amount = 1; amount <= total; amount++) {
+
+            foreach (int coin in coins) {
+
+                if (coin <= amount) {
+                    dp[amount] = Math.Min(dp[amount], dp[amount - coin] + 1);
+                }
             }
         }
-        
-        memo[amount] = res;
-        return res;
+
+        return dp[total] > total ? -1 : dp[total];
     }
 
-    public int CoinChange(int[] coins, int amount) {
+
+    // private Dictionary<int, int> memo = new Dictionary<int, int>();
+
+    // private int Dfs(int amount, int[] coins) {
+
+    //     if (amount == 0) 
+    //         return 0;
+
+            
+    //     if (amount < 0) 
+    //         return  int.MaxValue;
         
-        Array.Sort(coins);
-        Array.Reverse(coins);
+    //     if (memo.ContainsKey(amount)) 
+    //         return memo[amount];
 
-        int minCoins = Dfs(amount, coins);
+    //     int res = int.MaxValue;
+        
+    //     foreach (int coin in coins) {
+    //         if (coin <= amount) {
+                
+    //            int result = Dfs(amount - coin, coins);
 
-        return minCoins == int.MaxValue ? -1 : minCoins;
-    }
+    //            if( result != int.MaxValue){
+    //                 res = Math.Min(res, 1+ result);
+
+    //            }
+
+               
+    //         }
+    //     }
+        
+    //     memo[amount] = res;
+    //     return res;
+    // }
+
+    
 }
