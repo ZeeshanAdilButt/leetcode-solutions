6d9950aa4448a3a19621e133c1c45361789c8be1
Zeeshan Adil <zeeabutt@outlook.com>
2025-04-28 04:03:18 +0500

    Time: 0 ms (100.00%) | Memory: 28.6 MB (90.68%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cs
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cs
@@ -1,22 +1,6 @@
 public class Solution {
     public int Tribonacci(int n) {
 
-        int[] memo = new int[n + 1];
-
-        // if(n==0)
-        //     return 0;
-
-        // if(n <= 2)
-        //     return 1;
-
-        return  Tribonacci(n, memo);
-    
-        
-    }
-
-    public int Tribonacci(int n, int[] memo ) {
-
-       
 
         if(n==0)
             return 0;
@@ -24,18 +8,46 @@ public class Solution {
         if(n <= 2)
             return 1;
 
-             if(memo[n] != 0 )
-                return memo[n];
+        int[] memo = new int[n + 1];
 
-        int res = 0;
+        memo[0]= 0;
+        memo[1]=1;
+        memo[2]=1;
 
-        res+=   Tribonacci(n -1, memo );
-         res+=  Tribonacci(n -2, memo) ;
-         res+= Tribonacci(n -3, memo) ;
+        for (int i = 3; i < n +1; i++){
 
-        memo[n] = res;
+            memo[i] = memo[i-1] +  memo[i-2] +  memo[i-3];
+        }
 
-        return res;
+    
+
+        return memo[n];
+    
         
     }
+
+    // public int Tribonacci(int n, int[] memo ) {
+
+       
+
+    //     if(n==0)
+    //         return 0;
+
+    //     if(n <= 2)
+    //         return 1;
+
+    //          if(memo[n] != 0 )
+    //             return memo[n];
+
+    //     int res = 0;
+
+    //     res+=   Tribonacci(n -1, memo );
+    //      res+=  Tribonacci(n -2, memo) ;
+    //      res+= Tribonacci(n -3, memo) ;
+
+    //     memo[n] = res;
+
+    //     return res;
+        
+    // }
 }
