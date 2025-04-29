3102e70c7129715b5c83ccb45022c2297fdea0fe
Zeeshan Adil <zeeabutt@outlook.com>
2025-04-29 16:24:52 +0500

    Time: 1 ms (98.39%) | Memory: 44.8 MB (81.06%) - LeetSync

--- 139-word-break/word-break.cs
+++ 139-word-break/word-break.cs
@@ -4,12 +4,12 @@ public class Solution {
 
     private Dictionary<int, bool> memo;
 
-    public bool WordBreak(string s, List<string> wordDict) {
+    public bool WordBreak(string s, IList<string> wordDict) {
         memo = new Dictionary<int, bool> { { s.Length, true } };
         return Dfs(s, wordDict, 0);
     }
 
-    private bool Dfs(string s, List<string> wordDict, int i) {
+    private bool Dfs(string s, IList<string> wordDict, int i) {
         if (memo.ContainsKey(i)) {
             return memo[i];
         }
@@ -28,35 +28,35 @@ public class Solution {
     }
 
 
-    public bool WordBreak(string s, IList<string> wordDict) {
+    // public bool WordBreak(string s, IList<string> wordDict) {
 
-        bool[] dp = new bool[s.Length+1];
+    //     bool[] dp = new bool[s.Length+1];
 
-        for(int i=0; i<dp.Length; i++){
-            dp[i]= false;
-        }
+    //     for(int i=0; i<dp.Length; i++){
+    //         dp[i]= false;
+    //     }
         
-        dp[s.Length]=true;
+    //     dp[s.Length]=true;
 
-        for (int i = s.Length -1; i>=0; i--){
+    //     for (int i = s.Length -1; i>=0; i--){
 
-            foreach(string word in wordDict){
+    //         foreach(string word in wordDict){
                 
-               string currentRemaining = s.Substring(i);
+    //            string currentRemaining = s.Substring(i);
 
-                if(word.Length <= currentRemaining.Length && currentRemaining.StartsWith(word)){
+    //             if(word.Length <= currentRemaining.Length && currentRemaining.StartsWith(word)){
                     
-                    dp[i]= dp[i + word.Length];
+    //                 dp[i]= dp[i + word.Length];
                     
-                }
+    //             }
 
-                //if any match found break for other words
-                if(dp[i])
-                break;
-            }
-        }
+    //             //if any match found break for other words
+    //             if(dp[i])
+    //             break;
+    //         }
+    //     }
 
-        return dp[0];
+    //     return dp[0];
         
-    }
+    // }
 }
