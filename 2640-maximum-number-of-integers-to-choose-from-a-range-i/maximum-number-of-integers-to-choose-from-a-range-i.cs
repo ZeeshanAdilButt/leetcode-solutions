a757647c2e67bc624e01167adce78ad79da0110a
Zeeshan Adil <zeeabutt@outlook.com>
2025-05-05 04:50:24 +0500

    Time: 49 ms (75.00%) | Memory: 62.3 MB (38.89%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cs
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cs
@@ -1,38 +1,59 @@
+// using System;
+
+// public class Solution {
+//     public int MaxCount(int[] banned, int n, int maxSum) {
+//         Array.Sort(banned);  // Sort for binary search
+//         int count = 0;
+
+//         for (int num = 1; num <= n; num++) {
+//             if (IsBanned(banned, num)) continue;
+
+//             maxSum -= num;
+
+//             if (maxSum < 0) break;
+
+//             count++;
+//         }
+
+//         return count;
+//     }
+
+//     private bool IsBanned(int[] arr, int target) {
+//         int left = 0, right = arr.Length - 1;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (arr[mid] == target) return true;
+
+//             if (arr[mid] > target) {
+//                 right = mid - 1;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
 using System;
+using System.Collections.Generic;
 
 public class Solution {
     public int MaxCount(int[] banned, int n, int maxSum) {
-        Array.Sort(banned);  // Sort for binary search
-        int count = 0;
+        var bannedSet = new HashSet<int>(banned);
+        int sum = 0, count = 0;
 
-        for (int num = 1; num <= n; num++) {
-            if (IsBanned(banned, num)) continue;
+        for (int i = 1; i <= n; i++) {
+            if (bannedSet.Contains(i)) continue;
 
-            maxSum -= num;
+            if (sum + i > maxSum) break;
 
-            if (maxSum < 0) break;
-
+            sum += i;
             count++;
         }
 
         return count;
-    }
-
-    private bool IsBanned(int[] arr, int target) {
-        int left = 0, right = arr.Length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) return true;
-
-            if (arr[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return false;
     }
 }
