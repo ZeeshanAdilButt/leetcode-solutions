5e9c1b7621fe2ac605e5f2b9c5a5a20e259b0029
Zeeshan Adil <zeeabutt@outlook.com>
2025-05-05 03:52:42 +0500

    Time: 10 ms (100.00%) | Memory: 43.7 MB (54.24%) - LeetSync

---
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int SmallestDistancePair(int[] nums, int k) {
+        Array.Sort(nums);
+        int n = nums.Length;
+
+        int low = 0;
+        int high = nums[n - 1] - nums[0];
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            int count = CountPairsWithMaxDistance(nums, mid);
+
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    // Count number of pairs with distance <= maxDistance
+    private int CountPairsWithMaxDistance(int[] nums, int maxDistance) {
+        int count = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.Length; right++) {
+            while (nums[right] - nums[left] > maxDistance) {
+                left++;
+            }
+            count += right - left;
+        }
+
+        return count;
+    }
+}
