6821f28fe5be765fc4c32287a28476346b265e0d
Zeeshan Adil <zeeabutt@outlook.com>
2025-05-09 04:05:39 +0500

    Time: 0 ms (100.00%) | Memory: 46.3 MB (71.24%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cs
+++ 54-spiral-matrix/spiral-matrix.cs
@@ -1,37 +1,44 @@
 public class Solution {
 
-    
     public IList<int> SpiralOrder(int[][] matrix)
     {
         int rows = matrix.Length;
         int cols = matrix[0].Length;
 
         int row = 0;
-        int col = -1;
-
+        int col = 0;
         int direction = 1;
 
         List<int> result = new List<int>();
 
-        // Traverse the matrix in a spiral order
         while (rows > 0 && cols > 0)
         {
+            // Move horizontally (left → right OR right → left)
             for (int i = 0; i < cols; i++)
             {
-                col += direction;
                 result.Add(matrix[row][col]);
+                col += direction;
             }
+
+            col -= direction; // Step back to valid col
+            row += direction; // Step into next row
             rows--;
 
+            // Move vertically (top → bottom OR bottom → top)
             for (int i = 0; i < rows; i++)
             {
-                row += direction;
                 result.Add(matrix[row][col]);
+                row += direction;
             }
+
+            row -= direction; // Step back to valid row
+            col -= direction; // Step into next col
+            
             cols--;
 
-            direction *= -1;
+            direction *= -1; // Reverse direction
         }
+
 
         return result;
     }
