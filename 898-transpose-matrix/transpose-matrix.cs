37b855f12bca806069674e7f494f8ac8ce400c4e
Zeeshan Adil <zeeabutt@outlook.com>
2025-05-09 05:50:50 +0500

    Time: 0 ms (100.00%) | Memory: 51.9 MB (62.77%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.cs
@@ -0,0 +1,35 @@
+public class Solution {
+
+    public int[][] Transpose(int[][] matrix) {
+        
+        int ROWS = matrix.Length;
+        int COLS = matrix[0].Length;
+
+        // In-place transpose for square matrices
+        if (ROWS == COLS) {
+            for (int r = 0; r < ROWS; r++) {
+                for (int c = 0; c < r; c++) {
+                    int tmp = matrix[r][c];
+                    matrix[r][c] = matrix[c][r];
+                    matrix[c][r] = tmp;
+                }
+            }
+
+            return matrix;
+        }
+
+        // Create a new matrix for non-square case
+        int[][] res = new int[COLS][];
+        for (int i = 0; i < COLS; i++) {
+            res[i] = new int[ROWS];
+        }
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                res[c][r] = matrix[r][c];
+            }
+        }
+
+        return res;
+    }
+}
